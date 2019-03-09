9c40d5f546261fbb7f0f05a331c700c94907118d
Richard Garai <dargzero@gmail.com>
2019-03-09 18:33:40 +0100

    cleanup

--- BuildDocker.bat
+++
@@ -1,8 +0,0 @@
-title AnonServer 
-E:
-cd E:\Programming\Go\anonymization
-docker-compose down –v -t 3
-docker rm -f -v anonymization_server
-docker ps -a
-docker-compose up -d --build 
-docker logs -f  anonymization_server
