6fda21c613aa75ce7f350ab59860161b778b848b
Richard Garai <dargzero@gmail.com>
2019-03-10 21:21:40 +0100

    gps unit tests & refactor

--- anonbll/gps_test.go
+++ anonbll/gps_test.go
@@ -1,22 +1,72 @@
 package anonbll
 
 import (
-	"log"
+	"fmt"
 	"testing"
 )
 
-func TestCoordStrings(t *testing.T) {
-	format := FindFormat("-47°, 85°")
-	if format != "DD" {
-		log.Printf("DD_ERR")
-	}
-	format = FindFormat("N 47° 22' 23\", W 85°  22' 23\"")
-	if format != "DMS" {
-		log.Printf("DMS_ERR")
-	}
+func TestParseCoordinate(t *testing.T) {
 
-	lat, lon, _ := ReadCordsValue("-47°, 85°", "DD")
-	if lat != -47 || lon != 85 {
-		log.Printf("ERR")
+	t.Run("parse DD coordinates", func(t *testing.T) {
+		lat, lon, _ := ParseCoordinate("-47.35°, 85.55°")
+		if lat != -47.35 || lon != 85.55 {
+			t.Errorf("got (%v, %v)", lat, lon)
+		}
+	})
+
+	t.Run("parse DMS coordinates", func(t *testing.T) {
+		lat, lon, _ := ParseCoordinate("S 47° 25' 75\", W 85° 25' 75\"")
+		if lat != -47.625 || lon != -85.625 {
+			t.Errorf("got (%v, %v)", lat, lon)
+		}
+	})
+
+	t.Run("parse DMS partial", func(t *testing.T) {
+		lat, lon, _ := ParseCoordinate("S 47°, W 85°")
+		if lat != -47 || lon != -85 {
+			t.Errorf("got (%v, %v)", lat, lon)
+		}
+	})
+
+	t.Run("parse DMS semi-partial", func(t *testing.T) {
+		lat, lon, _ := ParseCoordinate("S 47° 25', W 85° 25'")
+		if lat != -47.416666666666664 || lon != -85.41666666666667 {
+			t.Errorf("got (%v, %v)", lat, lon)
+		}
+	})
+
+	t.Run("parse error", func(t *testing.T) {
+		_, _, err := ParseCoordinate("invalid format")
+		if err == nil {
+			t.Errorf("expected error, got nil")
+		}
+	})
+
+	overflows := []string{
+		"%s° 50°",
+		"50° %s°",
+		"S %s° 25' 75\", W 85° 25' 75\"",
+		"S 47° %s' 75\", W 85° 25' 75\"",
+		"S 47° 25' %s\", W 85° 25' 75\"",
+		"S 47° 25' 75\", W %s° 25' 75\"",
+		"S 47° 25' 75\", W 85° %s' 75\"",
+		"S 47° 25' 75\", W 85° 25' %s\"",
 	}
+	for _, test := range overflows {
+		t.Run("convert error", func(t *testing.T) {
+			coordinate := fmt.Sprintf(test, getBigNumberString())
+			lat, lon, err := ParseCoordinate(coordinate)
+			if err == nil {
+				t.Errorf("expected error, got (%f, %f)", lat, lon)
+			}
+		})
+	}
+}
+
+func getBigNumberString() string {
+	s := "1"
+	for i := 0; i < 100; i++ {
+		s += "00000"
+	}
+	return s
 }
