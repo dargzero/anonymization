28a9aab37288125a973738a5e7f8d89e3d69977e
Richard Garai <dargzero@gmail.com>
2019-03-23 18:55:34 +0100

    upload data tests

--- tests-integration/upload_test.go
+++ tests-integration/upload_test.go
@@ -1,19 +1,23 @@
+//+build integration
+
 package tests_integration
 
 import (
+	"encoding/json"
+	"github.com/dargzero/anonymization/anonmodel"
+	"github.com/dargzero/anonymization/swagger"
 	"testing"
 )
 
 var dataset = "/datasets/upload-test"
+var upload = "/upload"
 
 func TestApi_UploadSession(t *testing.T) {
-
-	uploadPath := "/upload"
 
 	t.Run("create upload session", func(t *testing.T) {
 		setup()
 		defer teardown()
-		status, _ := send("POST", uploadPath, "u_session.json")
+		status, _ := send("POST", upload, "u_session.json")
 		if status != 200 {
 			t.Errorf("unexpected status: %v", status)
 		}
@@ -22,12 +26,39 @@ func TestApi_UploadSession(t *testing.T) {
 	t.Run("existing upload session", func(t *testing.T) {
 		setup()
 		defer teardown()
-		send("POST", uploadPath, "u_session.json")
-		status, _ := send("POST", uploadPath, "u_session.json")
+		send("POST", upload, "u_session.json")
+		status, _ := send("POST", upload, "u_session.json")
 		if status != 400 {
 			t.Errorf("unexpected status: %v", status)
 		}
 	})
+}
+
+func TestApi_UploadData(t *testing.T) {
+
+	setup()
+	defer teardown()
+
+	t.Run("upload data to invalid session", func(t *testing.T) {
+		status, _ := send("POST", upload+"/invalid", "u_data_machine.json")
+		if status != 400 {
+			t.Errorf("unexpected status: %v", status)
+		}
+	})
+
+	t.Run("upload medical data", func(t *testing.T) {
+		_, sessRes := send("POST", upload, "u_session.json")
+		var session anonmodel.UploadSessionData
+		json.Unmarshal([]byte(sessRes), &session)
+		sessionPath := upload + "/" + session.SessionID + "?last=true"
+		status, body := send("POST", sessionPath, "u_data_medical.json")
+		var result swagger.UploadResponse
+		json.Unmarshal([]byte(body), &result)
+		if status != 200 || !result.InsertSuccessful || !result.FinalizeSuccessful {
+			t.Errorf("failed to upload: status=%v, result=%v", status, result)
+		}
+	})
+
 }
 
 func setup() {
